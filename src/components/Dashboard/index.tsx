43dd7b43c2f08e1e29f1636e814de7be16523c34
William Spada <williamrspada02@gmail.com>
2022-04-13 13:38:35 -0300

    using axios to request http

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,6 +2,26 @@
     import { Container } from "./styles";
     import { Summary } from "../Summary";
     import { TransactionsTable } from "../TransactionsTable";
+    import { createServer } from "miragejs";
+
+    createServer({
+        routes(){
+            this.namespace = 'api';
+
+            this.get('/transactions', () => {
+                return [
+                    {
+                        id: 1,
+                        title: 'Transaction 1',
+                        amount: 400,
+                        type: 'deposit',
+                        category: 'Food',
+                        createdAt: new Date()
+                    }
+                ]
+            });
+        }
+    });
 
     export function Dashboard(){
         return(
